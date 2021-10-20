7a1dde74e04a89917b8bfa535ab9b6b4a2b46432
claud <claud@DESKTOP-KB2SVN6>
2021-10-20 23:45:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace PentelescuClaudiu
+{
+    class SimpleWindow3D : GameWindow
+    {
+        const float rotation_speed = 180.0f;
+        float angle;
+        bool showCube = true;
+        KeyboardState lastKeyPress;
+        bool moveRight, moveLeft, goUp, goDown;
+
+        //private float mouseXLast;
+        //private float mouseYLast;
+        //private float mouseSLast;
+        //private Vector2 lastMousePos;
+
+        public SimpleWindow3D() : base(800, 600)
+        {
+            VSync = VSyncMode.On;
+            KeyDown += Keyboard_KeyDown;
+        }
+        //void ResetCursor()
+        //{
+        //    OpenTK.Input.Mouse.SetPosition(Bounds.Left + Bounds.Width / 2, Bounds.Top + Bounds.Height / 2);
+        //    lastMousePos = new Vector2(OpenTK.Input.Mouse.GetState().X, OpenTK.Input.Mouse.GetState().Y);
+        //}
+
+        void Keyboard_KeyDown(object sender, KeyboardKeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                this.Exit();
+
+            if (e.Key == Key.F11)
+                if (this.WindowState == WindowState.Fullscreen)
+                    this.WindowState = WindowState.Normal;
+                else
+                    this.WindowState = WindowState.Fullscreen;
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.ClearColor(Color.PaleTurquoise);
+            //GL.Enable(EnableCap.DepthTest);
+        }
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, Width, Height);
+
+            double aspect_ratio = Width / (double)Height;
+
+            Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)aspect_ratio, 1, 64);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspective);
+        }
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            base.OnUpdateFrame(e);
+
+            KeyboardState keyboard = OpenTK.Input.Keyboard.GetState();
+            MouseState mouse = OpenTK.Input.Mouse.GetState();
+            moveRight = false;
+            moveLeft = false;
+            goUp = false;
+            goDown = false;
+
+            // Se utilizeaza mecanismul de control input oferit de OpenTK (include perifcerice multiple, mai ales pentru gaming - gamepads, joysticks, etc.).
+            if (keyboard[OpenTK.Input.Key.Escape])
+            {
+                Exit();
+                return;
+            }
+            else if (keyboard[OpenTK.Input.Key.P] && !keyboard.Equals(lastKeyPress))
+            {
+                // Ascundere comandată, prin apăsarea unei taste - cu verificare de remanență! Timpul de reacțieuman << calculator.
+                if (showCube == true)
+                {
+                    showCube = false;
+                }
+                else
+                {
+                    showCube = true;
+                }
+            }
+            if (keyboard[OpenTK.Input.Key.Left])
+            {
+                //if (moveLeft)
+                //    moveLeft = false;
+                //else
+                    moveLeft = true;
+            }
+            if (keyboard[OpenTK.Input.Key.Right])
+            {
+                //if (moveRight)
+                //    moveRight = false;
+                //else
+                    moveRight = true;
+            }
+            lastKeyPress = keyboard;
+
+
+            //if (mouse[OpenTK.Input.MouseButton.Left])
+            //{
+            //    // Ascundere comandată, prin clic de mouse - fără testare remanență.
+            //    if (showCube == true)
+            //    {
+            //        showCube = false;
+            //    }
+            //    else
+            //    {
+            //        showCube = true;
+            //    }
+            //}
+        }
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Matrix4 lookat = Matrix4.LookAt(0, 1, 10, 0, 0, 0, 0, 1, 0);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref lookat);
+
+
+            //GL.Rotate(angle, 0.0f, 1.0f, 0.0f);
+
+            if (moveRight)
+            {
+                angle += rotation_speed * (float)e.Time;
+                GL.Rotate(angle, 0.0f, 1.0f, 0.0f);
+            }
+            if (moveLeft)
+            {
+                angle += rotation_speed * (float)e.Time;
+                GL.Rotate(angle, 0.0f, -1.0f, 0.0f);
+            }
+
+            // Exportăm controlul randării obiectelor către o metodă externă (modularizare).
+            if (showCube == true)
+            {
+                DrawCube();   
+            }
+
+            SwapBuffers();
+            //Thread.Sleep(1);
+        }
+
+        private void DrawCube()
+        {
+            GL.Begin(PrimitiveType.Quads);
+
+            GL.Color3(Color.DarkViolet);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+
+            GL.Color3(Color.DarkSeaGreen);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+
+            GL.Color3(Color.DarkRed);
+
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+
+            GL.Color3(Color.DarkTurquoise);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+
+            GL.Color3(Color.Magenta);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+
+            GL.Color3(Color.ForestGreen);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+
+            GL.End();
+        }
+        /// /////////////////////////////
+        //public void TrackMouse()
+        //{
+        //    this.mouseXLast = OpenTK.Input.Mouse.GetState().X;
+        //    this.mouseYLast = OpenTK.Input.Mouse.GetState().Y;
+        //    this.mouseSLast = OpenTK.Input.Mouse.GetState().WheelPrecise;
+        //}
+
+        static void Main(string[] args)
+        {
+            using (SimpleWindow3D example = new SimpleWindow3D())
+            {
+                example.Run(30.0, 0.0);
+            }
+        }
+    }
+}
